a097f2bd752bb6d9ee36874495a8aa3ef7e02ccd
Ginger-owl <r.s.uteshev@gmail.com>
2022-11-25 21:01:42 +0300

    feat: finish number tasks section

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -182,8 +182,15 @@ function getParallelepipedDiagonal(a, b, c) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  if (pow === 0) {
+    return num;
+  }
+  const ending = num % (10 ** pow);
+  if (ending >= (10 ** pow) / 2) {
+    return num + (10 ** pow - ending);
+  }
+  return num - ending;
 }
 
 /**
@@ -203,8 +210,17 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+
+  for (let i = 2; i < n; i += 1) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /**
@@ -222,8 +238,11 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  if (value === null || Number.isNaN(Number(value))) {
+    return def;
+  }
+  return Number(value);
 }
 
 module.exports = {
